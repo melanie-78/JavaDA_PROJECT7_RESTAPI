2f774003dc8ef1497346f91773ef28b1d53e9dec
Mel <adjohmelanie@gmail.com>
2022-09-29 11:30:10 +0200

    Mise en place du crud pour l'entité CurvePoint.

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -1,6 +1,7 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.service.CurvePointService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,15 +11,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class CurveController {
-    // TODO: Inject Curve Point service
+    // Inject Curve Point service
+    private CurvePointService curvePointService;
+
+    public CurveController(CurvePointService curvePointService) {
+        this.curvePointService = curvePointService;
+    }
 
     @RequestMapping("/curvePoint/list")
     public String home(Model model)
     {
-        // TODO: find all Curve Point, add to model
+        // find all Curve Point, add to model
+        List<CurvePoint> allCurvePoint = curvePointService.getAllCurvePoint();
+        model.addAttribute("listOfCurvePoint", allCurvePoint);
         return "curvePoint/list";
     }
 
@@ -29,26 +38,41 @@ public class CurveController {
 
     @PostMapping("/curvePoint/validate")
     public String validate(@Valid CurvePoint curvePoint, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Curve list
+        // check data valid and save to db, after saving return Curve list
+        if (!result.hasErrors()) {
+            curvePointService.saveCurvePoint(curvePoint);
+            model.addAttribute("listOfCurvePoint", curvePointService.getAllCurvePoint());
+            return "redirect:/curvePoint/list";
+        }
         return "curvePoint/add";
     }
 
     @GetMapping("/curvePoint/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-        // TODO: get CurvePoint by Id and to model then show to the form
+        // get CurvePoint by Id and to model then show to the form
+        CurvePoint curvePoint = curvePointService.getCurvePointById(id);
+        model.addAttribute("curvePoint", curvePoint);
         return "curvePoint/update";
     }
 
     @PostMapping("/curvePoint/update/{id}")
-    public String updateBid(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
-                             BindingResult result, Model model) {
-        // TODO: check required fields, if valid call service to update Curve and return Curve list
+    public String updateCurvePoint(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
+                                   BindingResult result, Model model) {
+        // check required fields, if valid call service to update Curve and return Curve list
+        if (result.hasErrors()) {
+            return "curvePoint/update";
+        }
+        curvePoint.setId(id);
+        curvePointService.saveCurvePoint(curvePoint);
+        model.addAttribute("listOfCurvePoint", curvePointService.getAllCurvePoint());
         return "redirect:/curvePoint/list";
     }
 
     @GetMapping("/curvePoint/delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Curve by Id and delete the Curve, return to Curve list
+    public String deleteCurvePoint(@PathVariable("id") Integer id, Model model) {
+        // Find Curve by Id and delete the Curve, return to Curve list
+        curvePointService.deleteCurvePoint(id);
+        model.addAttribute("listOfCurvePoint", curvePointService.getAllCurvePoint());
         return "redirect:/curvePoint/list";
     }
 }
---
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/CurvePointServiceImpl.java
@@ -0,0 +1,42 @@
+package com.nnk.springboot.service;
+
+import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.repositories.CurvePointRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CurvePointServiceImpl implements CurvePointService {
+    private CurvePointRepository curvePointRepository;
+
+    public CurvePointServiceImpl(CurvePointRepository curvePointRepository) {
+        this.curvePointRepository = curvePointRepository;
+    }
+
+    @Override
+    public List<CurvePoint> getAllCurvePoint() {
+        List<CurvePoint> allCurvePoint = curvePointRepository.findAll();
+        return allCurvePoint;
+    }
+
+    @Override
+    public void deleteCurvePoint(Integer curveId) {
+        CurvePoint curvePoint = curvePointRepository.findById(curveId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid curve Point id: " + curveId));
+        curvePointRepository.delete(curvePoint);
+    }
+
+    @Override
+    public CurvePoint saveCurvePoint(CurvePoint curvePoint) {
+        CurvePoint savedCurvePoint = curvePointRepository.save(curvePoint);
+        return savedCurvePoint;
+    }
+
+    @Override
+    public CurvePoint getCurvePointById(Integer id) {
+        CurvePoint curvePointById = curvePointRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid CurvePoint Id: "+ id));
+        return curvePointById;
+    }
+}
